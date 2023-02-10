d29e75e672222baf89f91e2d4b76f25d39d74fcb
Joshua <joshmack02@gmail.com>
2023-02-09 17:11:33 -0800

    Body section added and styled

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
 import Header from '@/components/Header'
+import Image from 'next/image'
+import {MagnifyingGlassIcon, MicrophoneIcon, BeakerIcon} from "@heroicons/react/24/solid"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +16,24 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <form className='flex flex-col items-center mt-40'>
+        <Image 
+        width="300" 
+        height="100" 
+        objectFit='cover'
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png" 
+        alt='Google Logo'
+        />
+        <div className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 hover:shadow-lg focus-within:shadow-lg px-5 py-3 rounded-full items-center sm:max-w-xl lg:max-w-2xl">
+          <MagnifyingGlassIcon className='h-5 text-gray-500 mr-3'/>
+          <input type="text" className='flex-grow focus:outline-none'/>
+          <MicrophoneIcon className='h-5'/>
+        </div>
+        <div className="flex flex-col sm:flex-row w-1/2 space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center">
+          <button className='btn'>Google Search</button>
+          <button className='btn'>Surprise Me!</button>
+        </div>
+      </form>
     </>
   )
 }
