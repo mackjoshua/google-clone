8c25697d6455bb9418d280c22680dc367dbca0c4
Joshua <joshmack02@gmail.com>
2023-02-09 17:37:28 -0800

    Search Input & redirect to search page

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,26 @@ import Header from '@/components/Header'
 import Image from 'next/image'
 import Footer from '@/components/Footer'
 import {MagnifyingGlassIcon, MicrophoneIcon, BeakerIcon} from "@heroicons/react/24/solid"
+import { useRouter } from 'next/router'
+import { useRef } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const router = useRouter();
+  const searchInputRef = useRef(null);
+
+  function search(event) {
+    event.preventDefault();
+    const term = searchInputRef.current.value;
+
+    if(!term.trim()) {
+      return
+    } else {
+      router.push(`/search?term=${term.trim()}`);
+    }
+  }
+
   return (
     <>
       <Head>
@@ -27,11 +43,11 @@ export default function Home() {
         />
         <div className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 hover:shadow-lg focus-within:shadow-lg px-5 py-3 rounded-full items-center sm:max-w-xl lg:max-w-2xl">
           <MagnifyingGlassIcon className='h-5 text-gray-500 mr-3'/>
-          <input type="text" className='flex-grow focus:outline-none'/>
+          <input ref={searchInputRef} type="text" className='flex-grow focus:outline-none'/>
           <MicrophoneIcon className='h-5'/>
         </div>
         <div className="flex flex-col sm:flex-row w-1/2 space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center">
-          <button className='btn'>Google Search</button>
+          <button onClick={search} className='btn'>Google Search</button>
           <button className='btn'>Surprise Me!</button>
         </div>
       </form>
