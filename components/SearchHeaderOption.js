dffefc08655348d3b316634ac799650fd1ec36c5
Joshua <joshmack02@gmail.com>
2023-02-10 18:02:34 -0800

    Search Option border coloring fixed

--- components/SearchHeaderOption.js
+++ components/SearchHeaderOption.js
@@ -8,7 +8,7 @@ export default function SearchHeaderOption({title, Icon, selected}) {
   return (
     <div 
     onClick={() => selectTab(title)}
-    className={`flex items-center space-x-1 border-b-4 border-transparent hover:text-blue-500 cursor-pointer hover:border-blue-500 pb-3 ${selected && "text-blue-500 border-blue-500"}`}>
+    className={`flex items-center space-x-1 border-b-4 border-transparent hover:text-blue-500 cursor-pointer hover:border-blue-500 pb-3 ${selected && "text-blue-500 border-blue-600"}`}>
         <Icon className="h-4"/>
         <p>{title}</p>
     </div>
