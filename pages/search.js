5a69157f1a9cad93769c9262d62c3df80875e094
Joshua <joshmack02@gmail.com>
2023-02-10 17:48:12 -0800

    Pagination buttons added

--- pages/search.js
+++ pages/search.js
@@ -22,8 +22,9 @@ export default function Search({response}) {
 }
 
 export async function getServerSideProps(context) {
-  const mockData = true;
-  const data = mockData ? Response : await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${context.query.searchType && "&searchType=image"}`).then((response) => response.json());
+  const startIndex = context.query.start || '1';
+  const mockData = false;
+  const data = mockData ? Response : await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${context.query.searchType && "&searchType=image"}&start=${startIndex}`).then((response) => response.json());
   return {
     props:{
       response: data
